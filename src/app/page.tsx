efde1c7824254f6d749d066e53705eb7037ec941
Zarlasht <zarlasht.barikzay1995@gmail.com>
2025-06-12 10:54:24 +0200

    global-css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@
 export default function HomePage() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-[var(--color-background)] text-center px-6 py-10">
-      <h1 className="text-4xl font-bold text-[var(--color-headings)] mb-4">
+      <h1 className="text-4xl font-bold  mb-4">
         Willkommen bei KleineSchätze ✨
       </h1>
 
-      <p className="text-lg text-[var(--color-texts)] max-w-xl mb-8">
+      <p className="text-lg max-w-xl mb-8">
         Halte besondere Momente deiner Kinder fest – ob Zeichnungen, erste Worte
         oder Lieblingszitate – alles an einem sicheren Ort.
       </p>
